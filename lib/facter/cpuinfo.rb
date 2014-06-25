653102bb9b9e8b053c0a772d65d22b9e5373e626
Lars Kellogg-Stedman <lars@redhat.com>
2014-06-25 16:48:45 -0400

    added some comments

--- lib/facter/cpuinfo.rb
+++ lib/facter/cpuinfo.rb
@@ -9,6 +9,8 @@ cpuinfo_text.each_line do |line|
         k = k.strip.gsub(' ', '_')
         v = v.strip
 
+        # Facts for each processor are stored under the
+        # processorN key.
         if k == 'processor'
             cur = {}
             cpuinfo["processor#{v}"] = cur
@@ -16,6 +18,9 @@ cpuinfo_text.each_line do |line|
 
         cur[k] = v
 
+        # Split CPU flags into individual facter facts.  These will 
+        # be named things like processor0_flag_fpu and will have the
+        # value '1'.
         if k == 'flags'
             v.split().each do |flag|
                 cur["flag_#{flag}"] = 1
@@ -26,6 +31,8 @@ end
 
 cpuinfo.each do |cpu, cpudata|
     cpudata.each do |k,v|
+        # Each fact will named processorN_FACT
+        # (e.g., processor0_vendor_id)
         Facter.add("#{cpu}_#{k}") do
             setcode do
                 v
